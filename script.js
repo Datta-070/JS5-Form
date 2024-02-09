62253de1288e7c3a1f07ab7e1104f36168298c36
Datta-070 <chinmaydatt003@gmail.com>
2024-02-09 12:26:06 +0530

    Responsiveness remaining

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent the form from submitting and page from refreshing
 
     let name = document.getElementById("name").value;
-    printName(name);
+    let password= document.getElementById("password").value;
+    let repassword= document.getElementById("repassword").value;
+    if(password !== repassword){
+        alert("Your passwords don't match !");
+    }
+    else{
+        printName(name);
+    }
 });
 
 
